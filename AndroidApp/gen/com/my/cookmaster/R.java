f317e5ef44e2855a818f1e15d8c7d419eb35a379
jimmygaoyang <jimmygaoyang@163.com>
2015-06-12 14:39:32 +0800

    APP可以显示多个图片了

--- AndroidApp/gen/com/my/cookmaster/R.java
+++ AndroidApp/gen/com/my/cookmaster/R.java
@@ -14,16 +14,46 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ble=0x7f050000;
-        public static final int net=0x7f050001;
+        public static final int flight_airline_tv=0x7f060001;
+        public static final int flight_from_tv=0x7f060003;
+        public static final int flight_lable_tv=0x7f060002;
+        public static final int flight_to_tv=0x7f060004;
+        public static final int imageView1=0x7f060005;
+        public static final int my_listview=0x7f060000;
+        public static final int removeView=0x7f060006;
+        public static final int ticke_expire_date_tv=0x7f06000a;
+        public static final int ticke_lable_tv=0x7f060008;
+        public static final int ticke_type_tv=0x7f060009;
+        public static final int ticket_name_tv=0x7f060007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int item_view_flight=0x7f030001;
+        public static final int item_view_image=0x7f030002;
+        public static final int item_view_ticket=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int ble=0x7f040002;
         public static final int hello_world=0x7f040001;
         public static final int net=0x7f040003;
+        public static final int removeView=0x7f040004;
+    }
+    public static final class style {
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+         */
+        public static final int AppBaseTheme=0x7f050000;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+         */
+        public static final int AppTheme=0x7f050001;
     }
 }
--- AndroidApp/src/com/my/cookmaster/MainActivity.java
+++ AndroidApp/src/com/my/cookmaster/MainActivity.java
@@ -1,6 +1,8 @@
 package com.my.cookmaster;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.http.Header;
 
@@ -9,59 +11,78 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.bluetooth.BluetoothDevice;
 
 import com.my.cookmaster.alogrithm.Alogrithm;
 import com.my.cookmaster.bus.BTtranse;
 import com.my.cookmaster.bus.HttpTranse;
+import com.my.cookmaster.view.listview.viewprovider.*;
+import com.my.cookmaster.view.listview.viewprovider.impl.*;
+import com.my.cookmaster.bean.bus_bean.*;
 import com.loopj.android.http.*;
 
 public class MainActivity extends Activity {
 	private BTtranse bttrans =new BTtranse();
 	private List<BluetoothDevice> devList =null;
 //	static AsyncHttpClient client = new AsyncHttpClient();
-	Button BleBtn;
-	Button NetBtn;
+//	Button BleBtn;
+//	Button NetBtn;
+	private ListView mListView;
+	private List<IItemBean> mList = new ArrayList<IItemBean>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		BleBtn = (Button)findViewById(R.id.ble);
-		NetBtn = (Button)findViewById(R.id.net);
+//		BleBtn = (Button)findViewById(R.id.ble);
+//		NetBtn = (Button)findViewById(R.id.net);
+		createData();
+		mListView = (ListView) findViewById(R.id.my_listview);
+		//不同之处在于多了一个provider集合，提供所有期望显示类型的provider class
+		//getView的实现在provider中实现，和在adapter中用法一样
+		List<Class<? extends IViewProvider>> providers = new ArrayList<Class<? extends IViewProvider>>();
+		providers.add(FlightOrderViewProvider.class);
+		providers.add(SticketOrderViewProvider.class);
+		providers.add(ImageViewProvider.class);
+		
+		MiltilViewListAdapter adpater = new MiltilViewListAdapter(getApplication(), mList, providers);
+		mListView.setAdapter(adpater);
+		MyScrollListener scrollListener = new MyScrollListener(adpater);
+		mListView.setOnScrollListener(scrollListener);
 		bttrans.open();
 		devList = bttrans.getCookDevice();
 		
-		BleBtn.setOnClickListener(new View.OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				bttrans.transeWithDevice(devList.get(0), "1234567".getBytes());
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}});
-		
-		NetBtn.setOnClickListener(new View.OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				HttpTranse Http = new HttpTranse();
-				byte [] sendBuf = "12345678".getBytes();			
-				int res = Http.TranseWithServer(MainActivity.this,"http://192.168.28.86/index.php/api/process", sendBuf);
-				if(res == 0)
-				{
-					String text = new String(Http.getRespData());
-					Log.d("cook",text);
-				}
-				
-				
-			}});
+//		BleBtn.setOnClickListener(new View.OnClickListener(){
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				bttrans.transeWithDevice(devList.get(0), "1234567".getBytes());
+//				try {
+//					Thread.sleep(2000);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				
+//			}});
+//		
+//		NetBtn.setOnClickListener(new View.OnClickListener(){
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				HttpTranse Http = new HttpTranse();
+//				byte [] sendBuf = "12345678".getBytes();			
+//				int res = Http.TranseWithServer(MainActivity.this,"http://192.168.28.86/index.php/api/process", sendBuf);
+//				if(res == 0)
+//				{
+//					String text = new String(Http.getRespData());
+//					Log.d("cook",text);
+//				}
+//				
+//				
+//			}});
 //		loop_thread.start();
 
 //			client.get("http://192.168.28.86/index.php/api/process", new AsyncHttpResponseHandler() {
@@ -113,4 +134,61 @@ public class MainActivity extends Activity {
 		
 		}
 	};
+	
+	private void createData(){
+		Random random = new Random();
+		
+		FlightOrder f = new FlightOrder();
+		f.airline = "东方航空-BS" + 1;
+		f.from = "北京";
+		f.to = "杭州";
+		mList.add(f);
+		TicketOrder t = new TicketOrder();
+		t.expireDate = "有效时间：2014-1-1至2014-5-5";
+		t.type = "类型：电子票";
+		t.title = "大裤衩" + 2;
+		mList.add(t);
+		ImageOder img = new ImageOder();
+		img.ImgURL = "http://recipe1.hoto.cn/pic/recipe/l/5b/b3/832347_53f806.jpg";
+		mList.add(img);
+		ImageOder img1 = new ImageOder();
+		img1.ImgURL = "http://avatar1.hoto.cn/f1/65/1992177_185.jpg?v=8";
+		mList.add(img1);
+		ImageOder img2 = new ImageOder();
+		img2.ImgURL = "http://recipe1.hoto.cn/pic/step/g_800/ff/4c/3362047.jpg";
+		mList.add(img2);
+		ImageOder img3 = new ImageOder();
+		img3.ImgURL = "http://recipe0.hoto.cn/pic/step/g_800/00/4d/3362048.jpg";
+		mList.add(img3);
+		ImageOder img4 = new ImageOder();
+		img4.ImgURL = "http://recipe1.hoto.cn/pic/step/g_800/01/4d/3362049.jpg";
+		mList.add(img4);
+		ImageOder img5 = new ImageOder();
+		img5.ImgURL = "http://recipe0.hoto.cn/pic/step/g_800/02/4d/3362050.jpg";
+		mList.add(img5);
+		ImageOder img6 = new ImageOder();
+		img6.ImgURL = "http://recipe1.hoto.cn/pic/step/g_800/03/4d/3362051.jpg";
+		mList.add(img6);
+		ImageOder img7 = new ImageOder();
+		img7.ImgURL = "http://recipe0.hoto.cn/pic/step/g_800/04/4d/3362052.jpg";
+		mList.add(img7);
+		
+		
+//		for(int i=0; i < 15; i++){
+//			int r = random.nextInt();
+//			if(r%2 == 0 ){
+//				FlightOrder f = new FlightOrder();
+//				f.airline = "东方航空-BS" + i;
+//				f.from = "北京";
+//				f.to = "杭州";
+//				mList.add(f);
+//			}else{
+//				TicketOrder t = new TicketOrder();
+//				t.expireDate = "有效时间：2014-1-1至2014-5-5";
+//				t.type = "类型：电子票";
+//				t.title = "大裤衩" + i;
+//				mList.add(t);
+//			}
+//		}
+	}
 }
